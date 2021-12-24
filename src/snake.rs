10d9ff1d142cb28def27bdf3ecab9748bf85c35d
Michael Dorst <m@mdorst.net>
2021-12-24 14:47:17 -0800

    Add movement

--- src/snake.rs
+++ src/snake.rs
@@ -12,15 +12,36 @@ impl Plugin for SnakePlugin {
 }
 
 fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
-    commands.spawn_bundle(SpriteBundle {
-        material: materials.head_material.clone(),
-        sprite: Sprite::new(Vec2::new(10.0, 10.0)),
-        ..Default::default()
-    });
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.head_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .insert(SnakeHead);
 }
 
-fn snake_move(mut query: Query<(&SnakeHead, &mut Transform)>) {
+fn snake_move(key: Res<Input<KeyCode>>, mut query: Query<(&SnakeHead, &mut Transform)>) {
     for (_, mut xform) in query.iter_mut() {
-        xform.translation.y += 1.0;
+        let left = key.pressed(KeyCode::Left);
+        let right = key.pressed(KeyCode::Right);
+        let up = key.pressed(KeyCode::Up);
+        let down = key.pressed(KeyCode::Down);
+        let x = if left && !right {
+            -2.0
+        } else if right && !left {
+            2.0
+        } else {
+            0.0
+        };
+        let y = if down && !up {
+            -2.0
+        } else if up && !down {
+            2.0
+        } else {
+            0.0
+        };
+        xform.translation.x += x;
+        xform.translation.y += y;
     }
 }
