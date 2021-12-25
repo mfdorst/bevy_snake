ae9e792bb2d5b440af43abf31c6cfd2e1a298061
Michael Dorst <m@mdorst.net>
2021-12-24 17:04:55 -0800

    Disable anti-aliasing

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,6 @@ fn main() {
             .with_system(size_scaling.system())
             .with_system(position_translation.system()),
     )
-    .insert_resource(Msaa { samples: 4 })
     .add_plugins(DefaultPlugins);
     #[cfg(target_arch = "wasm32")]
     app.add_plugin(bevy_webgl2::WebGL2Plugin);
